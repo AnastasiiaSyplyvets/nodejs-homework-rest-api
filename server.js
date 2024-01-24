958f4ef2510c8b8e8d1c6d901b2757a903d3a314
AnastasiiaSyplyvets <102216774+AnastasiiaSyplyvets@users.noreply.github.com>
2024-01-24 12:11:14 +0100

    1

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
-const app = require("./app");
+// const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
 
 // zAxWYJ38hFDKAy4t;
 
 //admin
+
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { DB_HOST } = process.env;
+
+console.log(DB_HOST);
+// const DB_HOST =
+//   "mongodb+srv://flizz:Kx59n8EmHVKAsj-@cluster0.lmg7zjz.mongodb.net/";
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
